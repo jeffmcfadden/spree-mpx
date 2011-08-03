a71f9368e93688e713b4ca58091c5e82c12b2f45
Ben Hutton <benhutton@gmail.com>
2011-08-03 13:04:09 -0400

    Removing spaces around parens

--- app/controllers/admin/mpx_controller.rb
+++ app/controllers/admin/mpx_controller.rb
@@ -10,19 +10,19 @@ class Admin::MpxController < Admin::BaseController
   def export
     mpx_exporter = MpxExporter.new(params)
 
-    zip_file_name = "mpx_export_#{Time.now.strftime( '%Y%m%d%k%M%S' )}.zip"
+    zip_file_name = "mpx_export_#{Time.now.strftime('%Y%m%d%k%M%S')}.zip"
     zip_temp_file = Tempfile.new(zip_file_name)
 
     Zip::ZipOutputStream.open(zip_temp_file.path) do |zip|
       [:donor_account_data, :donor_email_data, :gift_master_data, :gift_detail_data, :order_master_data, :order_detail_data].each do |file_to_export|
         csv_file_name = file_to_export.to_s.titleize.gsub(/ Data/, '').gsub(' ', '_').gsub('Email', 'EMailAddress') + ".csv" #Generates file names as requested
 
-        csv_temp_file = Tempfile.new( csv_file_name )
-        csv_temp_file.write( mpx_exporter.send( file_to_export ) )
+        csv_temp_file = Tempfile.new(csv_file_name)
+        csv_temp_file.write(mpx_exporter.send(file_to_export))
         csv_temp_file.fsync
 
-        zip.put_next_entry( csv_file_name )
-        zip.write IO.read( csv_temp_file.path )
+        zip.put_next_entry(csv_file_name)
+        zip.write IO.read(csv_temp_file.path)
 
         csv_temp_file.close
       end
